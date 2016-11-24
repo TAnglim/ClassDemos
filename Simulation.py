3296a91e1ee2e41ca22260692b0eea6b2a8d3316
TAnglim <TAnglim@Webucator.com>
2016-11-24 16:04:47 -0500

    Initial gaming simulation

---
+++ Simulation.py
@@ -0,0 +1,32 @@
+import statistics as stats
+from Die import Die
+
+class Simulation:
+    def __init__(self, fnct_to_run, iterations):
+        self._fnct_to_run = fnct_to_run
+        self._iterations = iterations
+        self._results = []
+        self.run()
+        
+    def run(self):
+        for i in range(self._iterations):
+            result = self._fnct_to_run()
+            self._results.append(result)
+    
+    def get_mean(self):
+        return stats.mean(self._results)
+    
+    def get_median(self):
+        return stats.median(self._results)
+    
+    def get_mode(self):
+        try:
+            return stats.mode(self._results)
+        except:
+            return None
+        
+die = Die()
+sim = Simulation(die.roll, 100000)
+print('Mean: {1}\tMedian: {1} \tMode: {2}'.upper().format(
+		sim.get_mean(), sim.get_median(), sim.get_mode()))
+		
