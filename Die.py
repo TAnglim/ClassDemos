c50deabf59da1e52d9b506a69dd6a4d8c780ce2e
TAnglim <TAnglim@Webucator.com>
2016-11-25 10:34:44 -0500

    Add WeightedDie and add main() for testing

--- Die.py
+++ Die.py
@@ -1,8 +1,49 @@
 import random  
+from collections import Counter
 
 class Die:
+    "A die"
     def __init__(self, sides=6):
-        self.sides = sides
+        """Creates a new standard die
+        
+        Keyword arguments:
+        sides (int) -- number of die sides.
+        """
+        if type(sides) != int or sides < 1:
+            raise Exception('sides must be a positive integer.')
+        self._sides = sides
+        self._rolls = []
+    
+    @property
+    def rolls(self):
+        "history of rolls"
+        return self._rolls
         
     def roll(self):
-        return random.randint(1, self.sides)
+        "Returns a value between 1 and the number of die sides."
+        roll = random.randint(1, self._sides)
+        self._rolls.append(roll)
+        return roll
+		
+
+def main():
+	die = Die()
+	rolls = []
+	
+	for i in range(100000):
+		roll = die.roll()
+		rolls.append(roll)
+		
+	c = Counter(rolls)
+	c_sorted = sorted(c.items())
+	print(c_sorted)
+	
+
+if __name__ == "__main__":
+	main()
+		
+		
+		
+		
+		
+		
---
+++ WeightedDie.py
@@ -0,0 +1,43 @@
+import random  
+from collections import Counter
+from Die import Die
+
+class WeightedDie(Die):
+    "A weighted die"
+    def __init__(self, weights, sides=6):
+        """Creates a new weighted die
+        
+        Keyword arguments:
+        sides (int) -- number of die sides.
+        weights (list) -- a list of integers holding the weights for each die side
+        """
+        if not isinstance(weights, list) or len(weights) != sides:
+            raise Exception('weights must be a list of length {}.'.format(sides))
+        super().__init__(sides)
+        self._weights = weights
+    
+    def roll(self):
+        """Returns a value between 1 and the number of die sides."""
+        options = []
+        for i in range(self._sides):
+            for j in range(self._weights[i]):
+                options.append(i+1)
+        roll = random.choice(options)
+        self._rolls.append(roll)
+        return roll
+		
+
+		
+def main():
+	die = WeightedDie(weights=[1,1,1,1,1,5])
+
+	for i in range(100000):
+		die.roll()
+		
+	c = Counter(die.rolls)
+	c_sorted = sorted(c.items())
+	print(c_sorted)
+	
+if __name__ == '__main__':
+	main()
+	
